6a72fbea7a9cb7a66b36b38e8ada9e5034e3284b
rmc3408 <rmc3408@protonmail.com>
2022-02-04 16:41:00 -0500

    video 136

--- meals/navigation/TabNavigator.js
+++ meals/navigation/TabNavigator.js
@@ -3,13 +3,23 @@ import React from 'react';
 import MealNavigator from './MealNavigator';
 import Favorites from '../screens/Favorites';
 import { Ionicons } from '@expo/vector-icons';
+import Colors from '../Colors';
 
 const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
     return (
-        <Tab.Navigator screenOptions={{ headerShown: false }}>
-            <Tab.Screen name="All" component={MealNavigator} />
+        <Tab.Navigator
+            screenOptions={{
+                headerShown: false,
+                tabBarActiveTintColor: Colors.secondary,
+            }}
+        >
+            <Tab.Screen
+                name="All"
+                component={MealNavigator}
+                options={{ tabBarIcon: () => <Ionicons name="ios-list" size={24} /> }}
+            />
             <Tab.Screen name="Favorites" component={Favorites} />
         </Tab.Navigator>
     );
